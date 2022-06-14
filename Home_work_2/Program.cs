dc436734bd1af88b3881fc7878516050aedc03f6
Ibaragim Darslanov <darslanov2020@inbox.ru>
2022-06-15 00:32:59 +0300

    Add Home work 2

---
+++ Home_work_2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число");
+int anyNamber = Convert.ToInt32(Console.ReadLine());
+string anyNamberText = Convert.ToString(anyNamber);
+
+if (anyNamberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + anyNamberText[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
